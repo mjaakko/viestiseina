d9d185517f9a7bf7cc3d076b1807095b57f5e8eb
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2018-09-06 16:53:05 +0300

    Add contructor

--- application/posts/models.py
+++ application/posts/models.py
@@ -15,4 +15,10 @@ class Post(db.Model):
 	create_time = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
 	modify_time = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp(), nullable=False)
 	user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+	content = db.Column(db.String(3000), nullable=False)
 	replies = db.relationship("Post")
+
+	def __init__(self, user_id, content, reply_to)
+		self.user_id = user_id
+		self.content = content
+		self.parent_id = reply_to
