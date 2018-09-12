090eefa526ceed736b851cdbc6d42e9a02ee3206
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2018-09-12 20:16:42 +0300

    Add support for updating posts

--- application/posts/views.py
+++ application/posts/views.py
@@ -13,6 +13,28 @@ def posts_index():
 def posts_form():
     return render_template("posts/new.html", form = PostForm())
 
+@app.route("/posts/update/<post_id>/")
+@login_required
+def posts_update(post_id):
+    post = Posts.query.get(post_id)
+
+    return render_template("posts/update.html", form = PostForm(post.content), post_id = post.id)
+
+@app.route("/posts/update/<post_id>/", methods=["POST"])
+@login_required
+def posts_update(post_id):
+    form = PostForm(request.form)
+
+    if not form.validate():
+        return render_template("posts/update.html", form = form, post_id = post_id)
+
+    post = Posts.query.get(post_id)
+    post.content = form.content.data
+
+    db.session().commit()
+
+    return redirect(url_for("posts_index"))
+
 @app.route("/posts/", methods=["POST"])
 @login_required
 def posts_create():
