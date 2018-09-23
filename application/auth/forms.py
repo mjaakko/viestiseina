40f2e296efc997a0bef60ab344b8643faab208a2
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2018-09-23 18:34:59 +0300

    Change minimum password length to 4 characters as 'demo' user had shorter password

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -3,7 +3,7 @@ from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
     username = StringField("Käyttäjätunnus", [validators.Regexp(regex="^[a-zA-Z0-9_]*$", message="Käyttäjätunnus voi sisältää vain kirjaimia ja numeroita sekä alaviivan"), validators.Length(min=2, message="Käyttäjätunnuksen tulee olla vähintään 2 merkkiä pitkä")])
-    password = PasswordField("Salasana", [validators.Regexp(regex="^[a-zA-Z0-9!@#\\-.+,/\"]*$", message="Salasana voi sisältää vain kirjaimia ja numeroita sekä joitain erikoismerkkejä"), validators.Length(min=5, message="Salasanan tulee olla vähintään 5 merkkiä pitkä")])
+    password = PasswordField("Salasana", [validators.Regexp(regex="^[a-zA-Z0-9!@#\\-.+,/\"]*$", message="Salasana voi sisältää vain kirjaimia ja numeroita sekä joitain erikoismerkkejä"), validators.Length(min=4, message="Salasanan tulee olla vähintään 4 merkkiä pitkä")])
 
     class Meta:
         csrf = False
