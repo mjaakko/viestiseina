ef460e13d5e4017e28168a669d9012204f33f63d
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2018-09-26 17:49:03 +0300

    Add model for hashtag

--- application/posts/models.py
+++ application/posts/models.py
@@ -1,5 +1,10 @@
 from application import db
 
+association_table = Table('post_hashtag', db.Model.metadata,
+    db.Column('post_id', Integer, db.ForeignKey('post.id')),
+    db.Column('hashtag_id', Integer, db.ForeignKey('hashtag.id'))
+)
+
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	# Parent id is the id of the post this post is a reply to
@@ -9,8 +14,21 @@ class Post(db.Model):
 	user_id = db.Column(db.Integer, db.ForeignKey("account.id"), nullable=False)
 	content = db.Column(db.String(3000), nullable=False)
 	replies = db.relationship("Post", cascade="all, delete-orphan")
+        hashtags = db.relationship("Hashtag",
+		secondary=association_table,
+		back_populates="posts")
 
 	def __init__(self, user_id, content, reply_to):
 		self.user_id = user_id
 		self.content = content
 		self.parent_id = reply_to
+
+class Hashtag(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(40), nullable=False, unique=True)
+        posts = db.relationship("Parent",
+               secondary=association_table,
+               back_populates="hashtags")
+
+        def __init__(self, name):
+                self.name = name
