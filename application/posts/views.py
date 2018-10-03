40cb03d58544d7591310185c1fe9a79f36a48f77
Jaakko Malkki <jaakko.malkki@helsinki.fi>
2018-10-03 19:49:02 +0300

    Send posts directly from posts list page

--- application/posts/views.py
+++ application/posts/views.py
@@ -6,12 +6,22 @@ from application.posts.forms import PostForm
 
 @app.route("/posts", methods=["GET"])
 def posts_index():
-    return render_template("posts/list.html", posts = Post.query.filter_by(parent_id=None).order_by(Post.create_time.desc()).all())
+    return render_template("posts/list.html", posts = Post.query.filter_by(parent_id=None).order_by(Post.create_time.desc()).all(), form = PostForm(), show = False)
 
-@app.route("/posts/new/")
+@app.route("/posts/", methods=["POST"])
 @login_required
-def posts_form():
-    return render_template("posts/new.html", form = PostForm())
+def posts_create():
+    form = PostForm(request.form)
+
+    if not form.validate():
+        return render_template("posts/list.html", posts = Post.query.filter_by(parent_id=None).order_by(Post.create_time.desc()).all(), form = form, show = True)
+
+    post = Post(current_user.id, form.content.data, None)
+  
+    db.session().add(post)
+    db.session().commit()
+  
+    return redirect(url_for("posts_index"))
 
 @app.route("/posts/update/<post_id>/")
 @login_required
@@ -44,21 +54,6 @@ def posts_delete(post_id):
     db.session.query(Post).filter(Post.id==post_id).delete()
     db.session.commit()
 
-    return redirect(url_for("posts_index"))
-
-@app.route("/posts/", methods=["POST"])
-@login_required
-def posts_create():
-    form = PostForm(request.form)
-
-    if not form.validate():
-        return render_template("posts/new.html", form = form)
-
-    post = Post(current_user.id, form.content.data, None)
-  
-    db.session().add(post)
-    db.session().commit()
-  
     return redirect(url_for("posts_index"))
 
 @app.route("/hashtags")
